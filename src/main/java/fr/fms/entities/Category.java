3db01f37f449ff4f473989b48a08a528fccafa16
DavidB <david.bernard@fms-ea.com>
2026-03-25 11:28:12 +0100

    first commit Category.java

---
+++ src/main/java/fr/fms/entities/Category.java
@@ -0,0 +1,33 @@
+package fr.fms.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Category implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  public Category() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
