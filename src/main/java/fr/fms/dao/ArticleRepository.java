3197fe09698555e1d5d68baa4838103c81890ab8
DavidB <david.bernard@fms-ea.com>
2026-03-26 10:47:57 +0100

    query  added to articlerepository

--- src/main/java/fr/fms/dao/ArticleRepository.java
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -3,7 +3,15 @@ package fr.fms.dao;
 import fr.fms.entities.Article;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
   public List<Article> findByBrand(String brand);
+
+  @Query("select A from Article A where A.brand like %:x% and A.price > :y")
+  public List<Article> searchArticles(
+    @Param("x") String kw,
+    @Param("y") double price
+  );
 }
